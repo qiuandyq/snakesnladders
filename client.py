2ba5470aadd689c69d3926598c8fb7a2af631f19
EdmondHuang1 <huang.edmond@hotmail.com>
2023-07-18 15:35:36 -0700

    Created skeleton code for client as well as sample background image of board.

---
+++ client.py
@@ -0,0 +1,60 @@
+import pygame
+import socket
+width = 800
+height = 800
+
+pygame.init()
+
+window = pygame.display.set_mode((width, height))
+
+bg = pygame.image.load('background board.jpg')
+bg = pygame.transform.scale(bg,(width,height))
+
+pygame.display.set_caption("Client")
+
+clientNumber = 0
+
+
+
+class Player():
+    def __init__(self, x, y, width, height, colour):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
+        self.rect = (x,y,width, height)
+
+    def draw (self, win):
+        pygame.draw.rect(window, self.colour, self.rect)
+
+    #use to move
+    #def move(self):
+
+
+def redrawWindow(window,player):
+    #window.fill((255,255,255))
+    player.draw(window)
+    pygame.display.update()
+
+
+
+
+def main():
+
+    running = True
+    p = Player(25,680,100,100,(255,0,0))
+
+    while running:
+
+        window.fill((0,0,0))
+        window.blit(bg, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+        redrawWindow(window,p)
+
+
+main()
